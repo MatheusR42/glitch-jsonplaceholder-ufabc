b101aa485196121670d130803604c072578eb1ff
Glitch (hello-express) <none>
2022-07-24 03:08:23 +0000

    🏣🍿 Checkpoint ./server.js:47069819/19803 ./package.json:47069819/56

--- server.js
+++ server.js
@@ -1,32 +1,14 @@
-// server.js
-// where your node app starts
-
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
 const express = require("express");
 const app = express();
 
-
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
-
-var buscaCep = require('busca-cep');
- 
-const OpenWeatherMapHelper = require('openweathermap-node');
-
-// For temperature in Fahrenheit use units=imperial
-// For temperature in Celsius use units=metric
-const helper = new OpenWeatherMapHelper(
-	{
-		APPID: 'd8ff33461e38363901c7cc712d49dca1',
-		units: "metric"
-	}
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
 );
-
-
 
 // make all the files in 'public' available
 // https://expressjs.com/en/starter/static-files.html
@@ -37,155 +19,65 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-
-app.post("/webhook", function(request, response) {
-
-   //response.json({"fulfillmentText" : "Previsao do tempo agora..."});
-  
+app.post("/webhook", function (request, response) {
   var intentName = request.body.queryResult.intent.displayName;
-  
-  if (intentName == "processo.seletivo") {
-    
-    //response.json({"fulfillmentText": "Processo seletivo"});
-   
-    response.json({
-      "fulfillmentMessages": [
-              {
-                "card": {
-                  "title": "Processo Seletivo",
-                  "subtitle": "Bem vindo ao nosso Processo seletivo",
-                  "imageUri": "https://cdn.glitch.global/40696ccc-9795-4f13-933d-12fb6ceec97b/processo-seletivo.jpg?v=1658277254993"
-                }
-              },
-              {
-                "text" :{
-                   "text": [
-                      "Os campi da UFABC estão sediados nos municípios de Santo André e São Bernardo do Campo"
-                  ]
-                }
-              },
-              {
-                "image":{
-                  
-                    "imageUri": "https://www.ufabc.edu.br/images/imagens_a_ufabc/campus-sa.jpg",
-                    "accessibilityText": "Campus em Santo André"
-                  
-                }
-              },
-              {
-                "image":{
-                  
-                    "imageUri": "https://www.ufabc.edu.br/images/imagens_a_ufabc/campus-sbc.jpg",
-                    "accessibilityText": "Campus em São Bernardo do Campo"
-                  
-                }
-              },
-              {
-                "text" :{
-                   "text": [
-                      "Nossos campi são os melhores do Brasil"
-                  ]
-                }
-              },
-              {
-                "text" :{
-                   "text": [
-                      "Voce quer participar do processo seletivo?"
-                  ]
-                }
-              }
-            ]
-     });
+  var parameters = request.body.queryResult.parameters;
+  response.json({"fulfillmentText" : "test"});
+  return;
 
-    
-    
-  }
   
-  
-    if ( intentName == "processo.seletivo - yes"  ) {
-    
-    var aluno_cep = request.body.queryResult.parameters['aluno-cep'];
-    
-    // http://viacep.com.br/
-    
-    buscaCep(aluno_cep, {sync: false, timeout: 1000})
-      .then(endereco => {
-      
-       var aluno_nome  = request.body.queryResult.parameters['aluno-nome'];
-       var aluno_cpf   = request.body.queryResult.parameters['aluno-cpf'];
-       var aluno_curso = request.body.queryResult.parameters['aluno-curso'];  
-      
-       var aluno_endereco = endereco.logradouro+"-"+endereco.bairro+","+endereco.localidade+"-"+endereco.uf+"--"+endereco.cep;
-        
-       response.json({"fulfillmentText": aluno_nome + ", CPF: " + aluno_cpf + ", curso: " + aluno_curso + ", o seu endereço completo é: " + aluno_endereco});
-       
-    })
-  
-  }
-  
-  if (intentName == "temperatura") {
-   
-    var cidade  = request.body.queryResult.parameters['cidade'];
-
-    //response.json({"fulfillmentText" : "Previsao do tempo para " + cidade});
-    
-    helper.getCurrentWeatherByCityName("" + cidade, (err, currentWeather) => {
-	    if (err) {
-		    console.log(err);
-        
-        response.json({"fulfillmentText": "Cidade ''" +  cidade + " '' nao encontrada"});
-	    }
-	    else {
-	     	console.log(currentWeather);
-        
-         var temperaturaAtual  = currentWeather.main.temp;
-         var temperaturaMaxima = parseInt(currentWeather.main.temp_max);
-         var temperaturaMinima = parseInt(currentWeather.main.temp_min);
-       
-        
-        /*response.json({"fulfillmentText" :
-          "Cidade: " + currentWeather.name + "\n" +
-          "Temperatura Atual: " + temperaturaAtual + "º" + "\n" +
-          "Temperatura Máxima: " + temperaturaMaxima + "º" + "\n" +
-          "Temperatura Mínima: " + temperaturaMinima
-        });*/
-        
-         response.json({"fulfillmentMessages":
-           [
-            {
-              "card": {
-                 "title": "Previsão do Tempo",
-                 "subtitle": "Cidade = " + currentWeather.name,
-                  "imageUri": "https://cdn.glitch.global/40696ccc-9795-4f13-933d-12fb6ceec97b/previsao.png?v=1657840458297"
-              }
-            },
-            {
-             "text" :{
-                "text": ["Temperatura atual = " + temperaturaAtual + "º"]
-             }
-            },
-            {
-             "text" :{
-                "text": ["Temperatura máxima = " + temperaturaMaxima + "º"]
-             }
-            },
-            {
-             "text" :{
-                "text": ["Temperatura mínima = " + temperaturaMinima + "º"]
-             }
-            }
-             
-           ]
-        });
 
 
-      }
-    });
-  }
- 
+  //     response.json({
+  //       "fulfillmentMessages": [
+  //               {
+  //                 "card": {
+  //                   "title": "Processo Seletivo",
+  //                   "subtitle": "Bem vindo ao nosso Processo seletivo",
+  //                   "imageUri": "https://cdn.glitch.global/40696ccc-9795-4f13-933d-12fb6ceec97b/processo-seletivo.jpg?v=1658277254993"
+  //                 }
+  //               },
+  //               {
+  //                 "text" :{
+  //                    "text": [
+  //                       "Os campi da UFABC estão sediados nos municípios de Santo André e São Bernardo do Campo"
+  //                   ]
+  //                 }
+  //               },
+  //               {
+  //                 "image":{
+
+  //                     "imageUri": "https://www.ufabc.edu.br/images/imagens_a_ufabc/campus-sa.jpg",
+  //                     "accessibilityText": "Campus em Santo André"
+
+  //                 }
+  //               },
+  //               {
+  //                 "image":{
+
+  //                     "imageUri": "https://www.ufabc.edu.br/images/imagens_a_ufabc/campus-sbc.jpg",
+  //                     "accessibilityText": "Campus em São Bernardo do Campo"
+
+  //                 }
+  //               },
+  //               {
+  //                 "text" :{
+  //                    "text": [
+  //                       "Nossos campi são os melhores do Brasil"
+  //                   ]
+  //                 }
+  //               },
+  //               {
+  //                 "text" :{
+  //                    "text": [
+  //                       "Voce quer participar do processo seletivo?"
+  //                   ]
+  //                 }
+  //               }
+  //             ]
+  //      });
+
 });
-
-
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
