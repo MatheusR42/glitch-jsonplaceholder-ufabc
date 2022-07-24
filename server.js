543240dd8f308d8ac6fdbdd79aadd127938ac69e
Glitch (hello-express) <none>
2022-07-24 04:08:20 +0000

    🚠🍕 Checkpoint ./server.js:47069819/9081

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const axios = require('axios');
+const axios = require("axios");
 
 var bodyParser = require("body-parser");
 app.use(bodyParser.json()); // to support JSON-encoded bodies
@@ -20,80 +20,93 @@ app.get("/", (request, response) => {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+function mapTodoToText(todos) {
+  return todos.map(({id, userId, title, completed}) => {
+    return {
+      text: {
+        text: [
+          ``
+        ],
+      },
+    };
+  });
+}
+
 app.post("/webhook", function (request, response) {
   var intentName = request.body.queryResult.intent.displayName;
   var parameters = request.body.queryResult.parameters;
-  console.log(intentName)
-  console.log(parameters)
-  response.json({"fulfillmentText" : "Aloooo!"});
-  
-  // Make a request for a user with a given ID
-  axios.get('/user?ID=12345')
-    .then(function (response) {
+  console.log(intentName);
+  const { number: userId, status } = parameters;
+  const basePath = "https://jsonplaceholder.typicode.com";
+  let path = "/todos";
+
+  if (userId) {
+    path = `/users/${userId}/todos`;
+  }
+
+  axios
+    .get(basePath + path)
+    .then(function ({ data }) {
       // handle success
-      console.log(response);
+      response.json({ fulfillmentText: "tasksss" });
+      console.log(data);
     })
     .catch(function (error) {
       // handle error
       console.log(error);
-    })
-    .then(function () {
-      // always executed
+      response.json({
+        fulfillmentText: "I could not understain what you said :(",
+      });
     });
-  return;
 
-  
+  //       response.json({
+  //         "fulfillmentMessages": [
+  //                 {
+  //                   "card": {
+  //                     "title": "Processo Seletivo",
+  //                     "subtitle": "Bem vindo ao nosso Processo seletivo",
+  //                     "imageUri": "https://cdn.glitch.global/40696ccc-9795-4f13-933d-12fb6ceec97b/processo-seletivo.jpg?v=1658277254993"
+  //                   }
+  //                 },
+  //                 {
+  //                   "text" :{
+  //                      "text": [
+  //                         "Os campi da UFABC estão sediados nos municípios de Santo André e São Bernardo do Campo"
+  //                     ]
+  //                   }
+  //                 },
+  //                 {
+  //                   "image":{
 
+  //                       "imageUri": "https://www.ufabc.edu.br/images/imagens_a_ufabc/campus-sa.jpg",
+  //                       "accessibilityText": "Campus em Santo André"
 
-//       response.json({
-//         "fulfillmentMessages": [
-//                 {
-//                   "card": {
-//                     "title": "Processo Seletivo",
-//                     "subtitle": "Bem vindo ao nosso Processo seletivo",
-//                     "imageUri": "https://cdn.glitch.global/40696ccc-9795-4f13-933d-12fb6ceec97b/processo-seletivo.jpg?v=1658277254993"
-//                   }
-//                 },
-//                 {
-//                   "text" :{
-//                      "text": [
-//                         "Os campi da UFABC estão sediados nos municípios de Santo André e São Bernardo do Campo"
-//                     ]
-//                   }
-//                 },
-//                 {
-//                   "image":{
+  //                   }
+  //                 },
+  //                 {
+  //                   "image":{
 
-//                       "imageUri": "https://www.ufabc.edu.br/images/imagens_a_ufabc/campus-sa.jpg",
-//                       "accessibilityText": "Campus em Santo André"
+  //                       "imageUri": "https://www.ufabc.edu.br/images/imagens_a_ufabc/campus-sbc.jpg",
+  //                       "accessibilityText": "Campus em São Bernardo do Campo"
 
-//                   }
-//                 },
-//                 {
-//                   "image":{
-
-//                       "imageUri": "https://www.ufabc.edu.br/images/imagens_a_ufabc/campus-sbc.jpg",
-//                       "accessibilityText": "Campus em São Bernardo do Campo"
-
-//                   }
-//                 },
-//                 {
-//                   "text" :{
-//                      "text": [
-//                         "Nossos campi são os melhores do Brasil"
-//                     ]
-//                   }
-//                 },
-//                 {
-//                   "text" :{
-//                      "text": [
-//                         "Voce quer participar do processo seletivo?"
-//                     ]
-//                   }
-//                 }
-//               ]
-//        });
-
+  //                   }
+  //                 },
+  //                 {
+  //                   "text" :{
+  //                      "text": [
+  //                         "Nossos campi são os melhores do Brasil"
+  //                     ]
+  //                   }
+  //                 },
+  //                 {
+  //                   "text" :{
+  //                      "text": [
+  //                         "Voce quer participar do processo seletivo?"
+  //                     ]
+  //                   }
+  //                 }
+  //               ]
+  //        });
 });
 
 // listen for requests :)
